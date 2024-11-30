1e3fe3356ccc233f0977b04aa473168d8db721ac
Anirban Kapil <anirbankapil@gmail.com>
2024-12-01 00:23:51 +0530

    verificationEmail setup

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -1,2 +1,22 @@
-import { resend } from "@/lib/resend.ts";
+import { resend } from "@/lib/resend";
 import VerificationEmail from "../../emails/VerificationEmail";
+import { ApiResponse } from "@/types/ApiResponse";
+
+export async function sendVerificationEmail (
+    email : string,
+    username : string,
+    verifyCode : string
+) : Promise<ApiResponse> {
+      try {
+            await resend.emails.send({
+            from: 'Acme <onboarding@resend.dev>',
+            to: email,
+            subject: 'MessageApp | Verification Code',
+            react: VerificationEmail({username,otp:verifyCode}),
+          });
+        return{success : true , message : "Verification email sent successfully"}
+      } catch (emailError) {
+        console.log("Error sending verification email. error : ", emailError)
+        return{success : false , message : "Failed to send verification email"}
+      }
+}
--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,5 +1,9 @@
+import { Message } from "@/model/User";
+
+
 export interface ApiResponse{
     success : boolean;
     message : string;
-    isAcceptingMessage ?: boolean
+    isAcceptingMessage ?: boolean;
+    messages? :Array<Message>
 }
