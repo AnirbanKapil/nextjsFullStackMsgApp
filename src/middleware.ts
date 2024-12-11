0530696e50aec6ed634abf5a26be1ac24a6db8d2
Anirban Kapil <anirbankapil@gmail.com>
2024-12-11 11:50:14 +0530

    bug fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-// export {default} from "next-auth/middleware" 
+export {default} from "next-auth/middleware" 
 import { getToken } from "next-auth/jwt"
 
 
@@ -20,8 +20,10 @@ export async function  middleware(request: NextRequest) {
     ) {
       return NextResponse.redirect(new URL('/dashboard', request.url))
   }
-
-  return NextResponse.redirect(new URL('/home', request.url))
+  if(!token && url.pathname.startsWith('/dashboard')){
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+  return NextResponse.next() 
 }
  
 
