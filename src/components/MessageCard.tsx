f85aca2779215bb7f5c67e67026e21d631534789
Anirban Kapil <anirbankapil@gmail.com>
2024-12-21 10:53:48 +0530

    fix MessageCard

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -23,6 +23,8 @@ import { Button } from "./ui/button"
 import { X } from "lucide-react"
 import { Message } from "@/model/User"
 import { useToast } from "@/hooks/use-toast"
+import axios from "axios"
+import { ApiResponse } from "@/types/ApiResponse"
 
 type MessageCardProps = {
   message : Message,
@@ -31,7 +33,16 @@ type MessageCardProps = {
   
 
 function MessageCard({message , onMessageDelete} : MessageCardProps) {
+  
   const toast = useToast()
+
+  const handleDeleteConfirm = async () => {
+    const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+    toast({
+      title : response.data.message
+    })
+    onMessageDelete(message._id)
+  }
   return (
     <Card>
     <CardHeader>
