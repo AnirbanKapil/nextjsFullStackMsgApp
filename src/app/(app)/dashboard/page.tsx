7e3e43adbdce2606089b5eb1f270a7d4a6fb735a
Anirban Kapil <anirbankapil@gmail.com>
2024-12-31 09:59:55 +0530

    DashBoard Logic

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -84,9 +84,28 @@ function DashBoard() {
     fetchMessages()
   },[session , setValue , fetchAcceptMessage , fetchMessages])
 
+
   const handleSwitchChange = async () => {
-    
+    try {
+      const response =  await axios.post<ApiResponse>('/api/accept-messages',{acceptMessages : !acceptMessages})
+      setValue("acceptMessages",!acceptMessages)
+      toast({
+        title : response.data.message,
+        variant : "destructive"
+      })
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>
+       toast({
+        title : "Error",
+        description : axiosError.response?.data.message || "Failed to fetch message settings",
+        variant : "destructive"
+    })
   }
+}
+
+if(!session || !session.user){
+  return <div>Please LoginIn</div>
+}
 
   return (
     <div>DashBoard</div>
