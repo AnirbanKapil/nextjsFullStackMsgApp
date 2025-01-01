a48db4e1859274674ffed9bd205c3a2c4b27a83f
Anirban Kapil <anirbankapil@gmail.com>
2025-01-01 20:28:03 +0530

    carousel setup

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent } from "@/components/ui/card"
+"use client"
+import { Card, CardContent, CardHeader } from "@/components/ui/card"
 import {
   Carousel,
   CarouselContent,
@@ -16,19 +17,26 @@ function Home() {
         <h1 className='text-3xl md:text-5xl font-bold'>Dive Into The World Of Anonymous Conversations</h1>
         <p className='mt-3 md:mt-4 text-base md:text-lg'>Explore Mystery Message - Where your identity remains a secret</p>
        </section>
-    <Carousel className="w-full max-w-xs">
+    <Carousel 
+    plugins={[Autoplay({delay : 2000})]}
+    className="w-full max-w-xs">
       <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index}>
+        {
+          messages.map((message,index)=> (
+            <CarouselItem key={index}>
             <div className="p-1">
               <Card>
+                <CardHeader>
+                  {message.title}
+                </CardHeader>
                 <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-4xl font-semibold">{index + 1}</span>
+                  <span className="text-lg font-semibold">{message.content}</span>
                 </CardContent>
               </Card>
             </div>
           </CarouselItem>
-        ))}
+          ))
+        }
       </CarouselContent>
       <CarouselPrevious />
       <CarouselNext />
